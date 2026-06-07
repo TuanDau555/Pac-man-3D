7a2011cf0d583e1ab525d85a89ed5520196f3787
TuanDau <nguyenmtuan505@gmail.com>
2026-06-07 19:38:30 +0700

    Add: Read only field

---
+++ Assets/Scripts/Utility/ReadOnlyAttribute.cs
@@ -0,0 +1,7 @@
+// Source - https://stackoverflow.com/a/77920674
+// Posted by Ujjwal Raut, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-06-07, License - CC BY-SA 4.0
+
+using UnityEngine;
+
+public class ReadOnlyAttribute : PropertyAttribute { }
